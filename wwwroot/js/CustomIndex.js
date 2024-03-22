41857c63a450b2abadf0c84852298856081e9430
AnushaPoosala <anusha.poosala@outlook.com>
2024-03-22 12:41:56 -0400

    Initial commit

---
+++ wwwroot/js/CustomIndex.js
@@ -0,0 +1,7 @@
+﻿alert("Our app is now onwards in skyblue color");
+
+// Select the body element
+var body = document.body;
+
+// Change the background color
+body.style.backgroundColor = "skyblue"; // You can set any color you want here
